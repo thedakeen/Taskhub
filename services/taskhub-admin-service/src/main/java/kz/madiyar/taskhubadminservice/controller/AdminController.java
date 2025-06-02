d7db27848de4d4713791fef3f035fc319360465f
Madiyar <123325330+Madiyar0405@users.noreply.github.com>
2025-06-02 14:32:12 +0500

    add logging Lombok

--- services/taskhub-admin-service/src/main/java/kz/madiyar/taskhubadminservice/controller/AdminController.java
+++ services/taskhub-admin-service/src/main/java/kz/madiyar/taskhubadminservice/controller/AdminController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin")
