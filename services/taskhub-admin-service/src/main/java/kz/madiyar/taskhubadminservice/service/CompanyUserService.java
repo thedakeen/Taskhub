322473243e05b043675fb9473de432bffea4e99e
Madiyar <123325330+Madiyar0405@users.noreply.github.com>
2025-06-02 15:08:49 +0500

    Update logging companyuser

--- services/taskhub-admin-service/src/main/java/kz/madiyar/taskhubadminservice/service/CompanyUserService.java
+++ services/taskhub-admin-service/src/main/java/kz/madiyar/taskhubadminservice/service/CompanyUserService.java
@@ -4,7 +4,9 @@ package kz.madiyar.taskhubadminservice.service;
 import kz.madiyar.taskhubadminservice.repository.CompanyUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CompanyUserService {
